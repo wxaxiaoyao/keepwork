550dd84dcd3fcbfd50d6a41890867625a575ea06
wxaxiaoyao <765485868@qq.com>
2017-10-30 15:16:58 +0800

    xxx

--- npl_mod/nws/router.lua
+++ npl_mod/nws/router.lua
@@ -11,10 +11,31 @@ function file_exist(filename)
 	return file ~= nil
 end
 
+local route = {}
+
+function route:new()
+	return {
+		next_map = {},  -- 子路径映射
+	}
+end
+
+function route:get_by_paths(root, paths)
+	root = root or root:new()
+	for _, path in ipairs(paths or {}) do
+		root.next_map[path] = root.next_map[path] or root:new()
+		root = root.next_map[path]
+	end
+
+	return root
+end
+
 local router = commonlib.gettable("nws.router")
+
 
 router.regexp_handler = {}
 router.normal_handler = {}
+router.controller_handler = {}
+router.tree_handler = route:new()
 router.controller_path = "controllers/"
 
 local method_list = {
@@ -25,37 +46,45 @@ local method_list = {
 	head = "head",
 	patch = "patch",
 	options = "options",
+	any = "any",
 }
 
-local function parse_path(path)
+function router:parse_path(path)
 	-- 是否正则path 正则串  路径参数名列表
-	local regexp_handler = self.regexp_handler
-	local normal_handler = self.normal_handler
-	local number = '[%d]+'
-	local string = '[%w]+'
-	local none = '[%d%w]+'
-	local regpath = "/"
+	local int = '([%%d]+)'
+	local number = '([%%d.]+)'
+	local string = '([%%w]+)'
+	local none = '([%%d%%w]+)'
+	local regpath = ""
 	local argslist = {}
 	local argscount = 1
 	local isregpath = false
+	local paths = {}
 	for word in string.gmatch(path, '([^/]+)') do
 		local argname = string.match(word, '(:[%w]+)')
 		local regstr = string.match(word, '(%(.*%))')
-		word = string.gsub(word, argname, "")
-		word = string.gsub(word, '(%(.*%))', regstr or none)
-		argslist[argscount] = argname
-		argscount = argscount + 1
-		
+		--print(argname, regstr)
+		paths[#paths+1] = word
 		if argname or regstr then
+			if argname and not regstr then
+				word = string.gsub(word, argname, none)
+			elseif argname and regstr then
+				word = string.gsub(word, argname, "")
+			end
+			word = string.gsub(word, '(%(int%))', int)
+			word = string.gsub(word, '(%(number%))', number)
+			word = string.gsub(word, '(%(string%))', string)
+			argslist[argscount] = argname or argscount
+			argscount = argscount + 1
 			isregpath = true
 		end
-
 		regpath = regpath .. '/' .. word 
 	end
-	return false, regstr, argslist
+	regpath = '^' ..regpath .. '$'
+	return isregpath, regpath, paths, argslist
 end
 
-local function get_controller(ctrl_name)
+function router:get_controller(ctrl_name)
 	if not ctrl_name or ctrl_name == "" then
 		return nil
 	end
@@ -68,28 +97,42 @@ local function get_controller(ctrl_name)
 	return ctrl
 end
 
+function router:get_handle(is_reg, path)
+	local handler = nil
+
+	if is_reg then
+		handler = self.regexp_handler
+	else
+		handler = self.normal_handler
+	end
+
+	handler[path] = handle[path] or {}
+
+	return handler[path]
+end
 -- path: url路劲
 -- controller: table|function
 -- handle: string 处理方式
 function router:router(path, controller, handles)
-	local is_reg, regstr, argslist = parse_path(path)
+	local is_reg, regstr, paths, argslist = self:parse_path(path)
 
 	local regexp_handler = self.regexp_handler
 	local normal_handler = self.normal_handler
+	local handle = self:get_handle(is_reg, path)
+	local route_handle = nil
 
-	local h = {
-		regstr = regstr,
-		path = path,
-		argslist = argslist,
-		controller = controller,
-		handle = handle,
-	}
+	handle.regstr = regstr
+	handle.path = path
+	handle.argslist = argslist
+	handle.controller = controller
+	handle.handle = handle,
+	handle.paths = paths
 
-	handles = handles or "all:"
+	handles = handles or "any:"
 	for handle in string.gmatch(handles, '([^,]+)') do
 		method, funcname = string.match(handle, "(.*):(.*)")
 		if not method or method == "" then
-			method = method_list[string.lower(handle)] or "all"
+			method = method_list[string.lower(handle)] or "any"
 			funcname = handle
 		end
 
@@ -100,51 +143,115 @@ function router:router(path, controller, handles)
 		h[method] = funcname
 	end
 
-	if is_reg then
-		regexp_handler[#regexp_handler+1] = h
-	else
-		normal_handler[path] = h
+	-- 注册控制器路由
+	if type(controller) == "table" and handles == nil then
+		self.controller_handler[path] = handle
+		route_handle = route:get_by_paths(self.tree_handler, paths)
+		route_handle.handle = handle
 	end
 
 	return self
 end
 
 function router:handle(path, ctx)
-	local normal_handle = self.normal_handler[path]
-	--local regexp_handler = self.regexp_handler
+	local normal_handler = self.normal_handler
+	local regexp_handler = self.regexp_handler
+	local tree_handler = self.tree_handler
 	local method = string.lower(ctx.request.method)
 	local paths = {}
 	local url_params = {}
+	local funcname = nil
+	local controller = nil
+	local handle = nil
+	local route_handle = nil
 
+	-- 处理回调执行
+	local handle_func = function(handle, ctx)
+		local funcname = handle[method] or handle["any"]
+		local controller = handle.controller
+		if funcname then
+			if type(controller)	== "function" then
+				return controller(ctx)
+			elseif type(controller) == "table" and controller[funcname] then
+				return (controller[funcname])(controller, ctx)
+			else
+				--error("controller type error")
+			end
+		end
+	end
+	
 	-- 普通完整匹配
-	local funcname = normal_handle[method] or normal_handle["all"]
-	local controller = normal_handle.controller
-	if funcname then
-		if type(controller)	== "function" then
-			return controller(ctx)
-		elseif type(controller) == "table" and controller[funcname] then
+	handle = normal_handler[path]
+	if handle then
+		return handle_func(handle, ctx)
+	end
+	
+	-- 控制器路径匹配
+	handle = self.controller_handler[(string.gsub(path, '/[%w%d]+', '')) or ""]
+	if handle then
+		paths = handle.paths
+		funcname = paths[#paths]
+		controller = handle.controller
+		if controller[funcname] then
 			return (controller[funcname])(controller, ctx)
-		else
-			--error("controller type error")
 		end
 	end
 
-	-- 控制器自动匹配
+	url_params = {}
+	handle = nil
+	for word in string.gmatch(path, '([^/]+)') do
+		paths[#paths+1] = word
+	end
+	for _, x in ipairs(paths) do
+		route_handle = tree_handler.next_map[x]
+		url_params[#url_params+1] = x
+		if route_handle and route_handle.handle then
+			handle = route_handle.handle
+			url_params = {}
+		end
+	end
+	if handle then
+		controller = handle.controller
+		funcname = url_params[1] or method
+		if controller[funcname] then
+			table.remove(url_params, 1)
+			ctx.request.url_params = url_params
+			return (controller[funcname])(controller, ctx)
+		end
+	end
+
+
+	url_params = {}
+	handle = nil
 	for word in string.gmatch(path, '([^/]+)') do
 		paths[#paths+1] = word
 		if #paths > 2 then
 			url_params[#url_params+1] = word
 		end
 	end
-	
+	-- 控制器自动匹配
 	ctx.request.url_params = url_params
-	controller = get_controller(paths[1]) 
+	controller = self:get_controller(paths[1]) 
 	funcname = paths[2] or method
 	if type(controllerj) == "table" and controller[funcname] then
 		return (controller[funcname])(controller, ctx)
 	end
 
 	-- 正则路由
+	for _, handle in ipairs(regexp_handler) do
+		url_params = {string.match(path, handle.regstr)}
+		if #url_params then
+			for i, v in ipairs(handle.argslist) do
+				url_params[v] = url_params[i] 
+			end
+			ctx.request.url_params = url_params
+			
+			return handle_func(handle, ctx)
+		end
+	end
+
+	print("no router match")
+	return nil
 end
 
 
--- npl_mod/nws/router_test.lua
+++ npl_mod/nws/router_test.lua
@@ -9,15 +9,37 @@ local ctx = {
 	}
 }
 
+-- 常用路由
+router:router("/api/v0/user/login", function(ctx) end) -- 任意方法请求
+router:router("/api/v0/user/login", function(ctx) end, 'any') -- 任意方法请求
+router:router("/api/v0/user/login", function(ctx) end, 'get')
+router:router("/api/v0/user/login", function(ctx) end, 'post')
+router:router("/api/v0/user/login", function(ctx) end, 'delete')
+router:router("/api/v0/user/login", function(ctx) end, 'put')
+router:router("/api/v0/user/login", function(ctx) end, 'head')
+router:router("/api/v0/user/login", function(ctx) end, 'patch')
+router:router("/api/v0/user/login", function(ctx) end, 'options')
+
+-- 控制器路由
+local userController = {}
+router:router("/api/v0/user", userController) -- get => userController:get()  post => userController:post() ...
+router:router("/api/v0/user/register", userController, "post:register") -- post =>  userController:register()
+router:router("/api/v0/user/1", userController, "get") -- get =>  userController:get()
+router:router("/api/v0/user/getByName", userController, "getByName") -- get =>  userController:getByName()
 
 router:router("/api/v0/user/login", function(ctx)
 	print("------------")
 	assert(ctx.request.method == "post", "method error:" .. ctx.request.method)
 end, "post")
 
+router:router("/api/:version/user/login", function(ctx)
+	print("------------")
+	assert(ctx.request.method == "post", "method error:" .. ctx.request.method)
+end, "post")
 --router:router("/api/v0/user/login", function(ctx)
 	--assert(ctx.request.method == "get", "method error")
 --end, "get")
+
 
 commonlib.console(router)
 
@@ -26,3 +48,7 @@ router:handle("/api/v0/user/login", ctx)
 ctx.request.method = "post"
 router:handle("/api/v0/user/login", ctx)
 
+
+local is_reg, regstr, argslist = router:parse_path("/api/:ver/:user(string)/(int)/login")
+print(regstr)
+print(string.match("/api/v2/user/3/login", regstr))
