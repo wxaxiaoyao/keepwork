5edb4d9a8062aa5e78ab0b788181e3032d95410d
xiaoyao <765485868@qq.com>
2017-11-03 23:50:46 +0800

    xx

--- model/demo_test.lua
+++ model/demo_test.lua
@@ -1,6 +1,11 @@
 
 local demo_model = nws.import("model/demo")
+local demo_test = nws.gettable("nws.model.demo_test")
 
+
+function demo_test:add_test()
+	nws.assert(demo_model.add(1,2) == 3)
+end
 
 
 
