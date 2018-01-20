2d6cbcea9ca45c750dc5ffe9b8156398b0eb58a9
xiaoyao <765485868@qq.com>
2018-01-20 18:45:43 +0800

    xx

--- code.lua
+++ code.lua
@@ -1,3 +1,4 @@
 
 
 os.execute("cd /root/workspace/lua/keepwork;git reset --hard HEAD;  git pull origin master; r.js -o build.js; cd npl_packages/nws; git reset --hard HEAD; git pull origin master")
+
